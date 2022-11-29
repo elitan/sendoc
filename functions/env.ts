6d14fa7d8a596ada0e71361822314582238eb10d
Johan Eliasson <johan@eliasson.me>
2022-11-29 12:53:42 +0100

    env

--- functions/env.ts
+++ functions/env.ts
@@ -1,10 +1,7 @@
-import { Request, Response } from 'express'
+import { Request, Response } from "express";
 
 const handler = (req: Request, res: Response) => {
-  console.log('env vars:')
-  console.log(process.env)
+  res.status(200).send(`env vars: ${JSON.stringify(process.env, null, 2)}`);
+};
 
-  res.status(200).send(`MY_TEST: ${process.env.MY_TEST}`)
-}
-
-export default handler
+export default handler;
