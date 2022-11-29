93488b22e925fc0de797659bcd7af579a3da19a9
Johan Eliasson <johan@eliasson.me>
2022-11-29 12:57:40 +0100

    formatting

--- functions/env.ts
+++ functions/env.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 const handler = (req: Request, res: Response) => {
-  res.status(200).send(`env vars: ${JSON.stringify(process.env, null, 2)}`);
+  res.status(200).json(JSON.stringify(process.env, null, 2));
 };
 
 export default handler;
