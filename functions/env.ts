3c32562d61a60152e5aa5f956dc8391774899d79
Johan Eliasson <johan@eliasson.me>
2022-12-16 08:11:04 +0100

    update

--- functions/env.ts
+++ functions/env.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 const handler = (req: Request, res: Response) => {
+  console.log("test from functions");
   res.status(200).json(JSON.stringify(process.env, null, 2));
 };
 
